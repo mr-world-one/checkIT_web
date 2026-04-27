e1e609c7268c2f1f23ce563f30a7d3ae0c1e0efb
Maxym <makko.22mv@gmail.com>
2026-04-27 08:57:57 +0300

    add test error to test ci/cd

--- CheckIT.Tests/Controllers/AccountControllerTests.cs
+++ CheckIT.Tests/Controllers/AccountControllerTests.cs
@@ -73,17 +73,17 @@ public class AccountControllerTests
     [Fact]
     public void Login_Get_WhenAnonymous_ReturnsViewWithModel()
     {
-        var userManager = tttCreateUserManager();
+        var userManager = CreateUserManager();
         var signInManager = CreateSignInManager(userManager.Object);
         var logger = new Mock<IAppLogger>();
 
-        var controller = CreateController(userManager, signInManager, logger);
+    //    var controller = CreateController(userManager, signInManager, logger);
 
-        var result = controller.Login();
+    //    var result = controller.Login();
 
-        var view = result.Should().BeOfType<ViewResult>().Which;
-        view.Model.Should().BeOfType<LoginViewModel>();
-    }
+    //    var view = result.Should().BeOfType<ViewResult>().Which;
+    //    view.Model.Should().BeOfType<LoginViewModel>();
+    //}
 
     [Fact]
     public async Task Login_Post_WhenModelInvalid_ReturnsViewSameModel()
